2bbe5f4d61bfbc31c054aa49c4708b02717c941d
Somsubhra <134898829+somsubhrad@users.noreply.github.com>
2023-08-11 18:54:48 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,1 +1,19 @@
+import streamlit as st
 
+def largest_num(x, y, z):
+    largest = x
+    if y > largest:
+        largest = y
+    if z > largest:
+        largest = z
+    return largest
+
+st.title("App for finding the largest among three given numbers - by Somsubhra")
+
+x = st.number_input("Enter the first number: ")
+y = st.number_input("Enter the second number: ")
+z = st.number_input("Enter the third number: ")
+
+if st.button("Compute the largest number"):
+    result = find_largest(x, y, z)
+    st.write("The largest number among the three provided inputs is:", result)
